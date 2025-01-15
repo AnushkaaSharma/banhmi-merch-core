6b57414e4d4fdff5e96009e4635ec93654f55843
Anushkaa Sharma <sanushkaa4@gmail.com>
2025-01-15 12:09:38 -0500

    import Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react'
+import Navigation from './customer/components/Navigation';
+
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+    <div className="">
+      <Navigation />
     </div>
+      
   );
 }
 
