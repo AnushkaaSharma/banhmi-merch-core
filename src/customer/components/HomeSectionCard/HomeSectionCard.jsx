3e93e2bffa3e124d3604c04d7bcb2a8e01369665
Anushkaa Sharma <sanushkaa4@gmail.com>
2025-01-28 23:26:37 -0500

    uppdate category card

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const HomeSectionCard = ({title, description, image}) => {
   return (
-    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[16rem] mx-3'>
+    <div>
+      <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg overflow-hidden w-[16rem] mx-3'>
         <div className='h-[13rem] w-[10rem]'>
             <img className="object-cover object-top w-full h-full" 
             src="" alt="" />
@@ -12,7 +13,23 @@ const HomeSectionCard = ({title, description, image}) => {
             <h3 className="text-lg font-medium text-gray-900">{title}</h3>
             <p className="mt-2 text-sm text-gray-500">{description}</p>
         </div>
+      </div>
+
+      <div className='cursor-pointer grid grid-cols-2 gap-4'>
+        <div className='h-[13rem] w-[10rem]'>
+            <img className="object-cover object-top w-full h-full" 
+            src={image} alt="" />
+        </div>
+
+        {/* <div className='p-4'>
+            <h3 className="text-lg font-medium text-gray-900">{title}</h3>
+            <p className="mt-2 text-sm text-gray-500">{description}</p>
+        </div> */}
+
+      </div>
     </div>
+
+    
   )
 }
 
--- src/customer/components/ShopByCategory/ShopByCategoryData.js
+++ src/customer/components/ShopByCategory/ShopByCategoryData.js
@@ -1,13 +1,13 @@
 const categories = [
     {
         title: "Noodles",
-        description: "Vermicelli Rice Noodles",
-        image: "/path/to/noodles.jpg",
+        description: "Vermicelli Rice Noodles, Glass Noodles, Pho Noodles",
+        image: "/assets/images/Category Card.png",
     },
     {
-        title: "Sandwiches",
-        description: "Banh Mi with fresh vegetables",
-        image: "/path/to/sandwiches.jpg",
+        title: "Sauces",
+        description: "Sriracha, Thai Sweet Chili",
+        image: "/path/to/sauces.jpg",
     }
 ]
 
