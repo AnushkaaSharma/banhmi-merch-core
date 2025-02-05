5822a13f31a34ebf06e7035ad6972971cde7ffb5
Anushkaa Sharma <sanushkaa4@gmail.com>
2025-02-05 13:42:48 -0500

    update shop by category section

--- src/customer/components/ShopByCategory/ShopByCategory.jsx
+++ src/customer/components/ShopByCategory/ShopByCategory.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
-import categories from './ShopByCategoryData'; 
+import categories from './ShopByCategoryData'
+import divider from '../../../assets/images/divider.png'
+import noodles from '../../../assets/images/Category Card.png'
+import sauce from '../../../assets/images/Category Card (2).png'
 
 const ShopByCategory = () => {
   return (
-    <div className="bg-gray-100 py-8">
-      <h2 className="text-2xl font-bold text-center mb-6">Shop by Category</h2>
-      <div className="flex justify-center flex-wrap">
-        {categories.map((category, index) => (
-          <HomeSectionCard
-            key={index}
-            title={category.title}
-            description={category.description}
-            image={category.image}
-          />
-        ))}
+    <div className="my-16 mx-28">
+      <h4 className="text-h4 text-semibold font-heading text-neutral-900 text-center pb-5">
+        Shop By Category
+      </h4>
+      <img className="w-48 h-auto mx-auto" src={divider} alt="Divider" />
+      <div className="grid grid-cols-2 gap-5 my-10">
+        <div className="relative h-full w-full">
+          {/* Background Image */}
+          <img className="h-full w-full object-cover" src={noodles} alt="Noodles" />
+          {/* Content (Bottom Left) */}
+          <div className="absolute bottom-0 left-0 p-6 text-neutral-100">
+            <h2 className="text-h5 font-semibold pb-2">Noodles</h2>
+            <a className="text-p py-2 font-regular border-b-2 border-solid border-neutral-100" href="#">View All</a>
+          </div>
+        </div>
+
+        {/*Second Category*/}
+        <div className="relative h-full w-full">
+          {/* Background Image */}
+          <img className="h-full w-full object-cover" src={sauce} alt="Sauce" />
+          {/* Content (Bottom Left) */}
+          <div className="absolute bottom-0 left-0 p-6 text-neutral-100">
+            <h2 className="text-h5 font-semibold pb-2">Noodles</h2>
+            <a className="text-p py-2 font-regular border-b-2 border-solid border-neutral-100" href="#">View All</a>
+          </div>
+        </div>
+      
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,47 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // Ensures Tailwind scans all your React components
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '300px',
+        'md': '1024px',
+        'lg': '1280px',
+      },
+    },
+    colors: {
+      neutral: {
+        '100': '#FAFAFA',
+        '200': '#F4F4F4',
+        '300': '#ECECEC',
+        '400': '#D8D8D8',
+        '500': '#C2C2C2',
+        '600': '#A1A1A1',
+        '700': '#7B7B7B',
+        '800': '#484848',
+        '900': '#2C2C2C',
+      },
+      primary: {
+        '500': '#DB9423',
+      },
+    },
+    fontSize: {
+      'sm': ['14px', '20px'],
+      'p': ['16px', '28px'],
+      'h6': ['20px', '32px'],
+      'h4': ['32px', '44px'],
+      'h5': ['24px', '32px'],
+      'h2': ['48px', '52px'],
+    },
+    fontWeight: {
+      light: 300,
+      regular: 400,
+      semibold: 600,
+      bold: 700,
+    },
+    fontFamily: {
+      body: ['Poppins'],
+      heading: ['Abril Fatface'],
+    },
   },
   plugins: [],
 };
