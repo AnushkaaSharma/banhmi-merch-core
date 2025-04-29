088ac4daeb940ad7017913ed95a0e163a939c4c9
Anushkaa Sharma <sanushkaa4@gmail.com>
2025-04-29 12:12:39 -0400

    Changes to navigation bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navigation from './customer/components/Navigation';
+import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './customer/pages/HomePage/HomePage';
 import ShopByCategory from './customer/components/ShopByCategory/ShopByCategory'
 import Product from './customer/components/Product/Product'
